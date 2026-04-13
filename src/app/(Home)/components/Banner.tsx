ebd0322957e39e9c7a154eb131137c6083af5eb8
indranil <ibera866@gmail.com>
2026-04-13 16:57:25 +0530

    move main page to Home group

--- src/app/(Home)/components/Banner.tsx
+++ src/app/(Home)/components/Banner.tsx
@@ -1,15 +1,13 @@
 import Image from 'next/image'
-import PaperImage from '../../public/paper-bg-image.jpg'
-import HandBookImage from '../../public/notebook-on-one-hand-holding.jpg'
 
 function Banner() {
   return (
     <div className='w-[50%]  mx-auto rounded-[10px]'>
         <div className='relative max-h-25 w-full flex items-center'>
-            <Image className='absolute h-25 w-full -z-1 rounded-[5px]' src={PaperImage} alt='background-image'/>
+            <Image className='absolute -z-1 rounded-[5px]' fill src={'/paper-bg-image.jpg'} alt='background-image'/>
             <div className='flex justify-around items-center w-full'>
                 <h2 className='bg-red w-[50%]' >Create, Share and Trade Your Favourite Reads...</h2>
-                <Image className='w-20 rounded-[5px]' src={HandBookImage} alt='banner-image' />
+                <Image className=' rounded-[5px]' width={70} height={70} src={'/notebook-on-one-hand-holding.jpg'} alt='banner-image' />
             </div> 
         </div>
     </div>
